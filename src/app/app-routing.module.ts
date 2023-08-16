c7cac094b065bbc5faceb20f63dfb389f146b6ce
kleiton <kleiduda@gmail.com>
2023-08-16 00:27:36 -0300

    fix(Route): aplicando rota principal para calculadora

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'calculadora-cdb', loadChildren: () => import('./components/cdb-calculator/cdb-calculator.module').then(m => m.CdbCalculatorModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'calculadora-cdb', pathMatch: 'full' },
+  { path: 'calculadora-cdb', loadChildren: () => import('./components/cdb-calculator/cdb-calculator.module').then(m => m.CdbCalculatorModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
